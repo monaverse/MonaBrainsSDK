7802281ed2ab79f2330b2031459870560f4abfc0
Alejandro Azcona <alejandro@monaverse.com>
2024-04-03 14:49:41 -0600

    Added UseGravity tile

---
+++ Runtime/Brains/Tiles/Actions/Physics/UseGravityInstructionTile.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System;
+using Mona.SDK.Brains.Core.Tiles;
+using Mona.SDK.Brains.Core;
+using Mona.SDK.Brains.Core.Brain;
+using Mona.SDK.Brains.Core.Enums;
+using Mona.SDK.Core.State.Structs;
+
+namespace Mona.SDK.Brains.Tiles.Actions.Physics
+{
+    [Serializable]
+    public class UseGravitysInstructionTile : InstructionTile, IActionInstructionTile, IInstructionTileWithPreload,
+        IRigidbodyInstructionTile
+    {
+        public const string ID = "UseGravity";
+        public const string NAME = "Use Gravity";
+        public const string CATEGORY = "Physics";
+        public override Type TileType => typeof(UseGravitysInstructionTile);
+
+        [SerializeField] private bool _value;
+        [SerializeField] private string _valueValueName;
+        [BrainProperty(true)] public bool Value { get => _value; set => _value = value; }
+        [BrainPropertyValueName("Value", typeof(UseGravitysInstructionTile))] public string ValueValueName { get => _valueValueName; set => _valueValueName = value; }
+
+        private IMonaBrain _brain;
+
+        public UseGravitysInstructionTile() { }
+
+        public void Preload(IMonaBrain brainInstance)
+        {
+            _brain = brainInstance;
+        }
+
+        public override InstructionTileResult Do()
+        {
+            if (!string.IsNullOrEmpty(_valueValueName))
+                _value = _brain.Variables.GetBool(_valueValueName);
+
+            if (_brain != null)
+            {
+                _brain.Body.SetUseGravity(_value);
+                return Complete(InstructionTileResult.Success);
+            }
+            return Complete(InstructionTileResult.Failure, MonaBrainConstants.INVALID_VALUE);
+        }
+
+    }
+}
