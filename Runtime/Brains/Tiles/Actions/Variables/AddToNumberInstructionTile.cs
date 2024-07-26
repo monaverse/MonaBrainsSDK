3bb168db6c7bf0bd1608aa37e60a7d9d919770ff
Alejandro Azcona <alejandro@monaverse.com>
2024-07-26 14:16:53 -0600

    - Fixed issue with AddToNumberInstructionTile class.

--- Runtime/Brains/Tiles/Actions/Variables/AddToNumberInstructionTile.cs
+++ Runtime/Brains/Tiles/Actions/Variables/AddToNumberInstructionTile.cs
@@ -6,9 +6,6 @@ namespace Mona.SDK.Brains.Tiles.Actions.Variables
     [Serializable]
     public class AddToNumberInstructionTile : SetNumberToInstructionTile
     {
-        public const string ID = "AddToNumber";
-        public const string NAME = "Add To Number";
-        public const string CATEGORY = "Variables";
         public override Type TileType => typeof(AddToNumberInstructionTile);
 
         public AddToNumberInstructionTile() { }
